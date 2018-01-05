7bb68f0a8406d09064053c593e73aa62e038274d
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-04 20:54:56 -0800

    Updated for heroku deployment

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 //Using Handlebars (hbs) as the View Engine (Or Template Engine)
@@ -52,6 +53,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
