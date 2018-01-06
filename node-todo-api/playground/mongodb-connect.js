6baa79ba39f2be231b6a5940d191030f0ffe6bc7
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-06 08:09:43 -0800

    Added new connection to mongo db, added a few collections

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Somethign to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // })
+
+db.collection('Users').insertOne({
+  name: "Chris",
+  age: 26,
+  location: "Irvine"
+}, (err, result) => {
+  if (err) {
+    return console.log('Unabl to insert user', err);
+  }
+  console.log(JSON.stringify(result.ops, undefined, 2));
+})
+
+  db.close();
+});
