1c476e4bb51c44f1252eba4ec6055d9ea12b7aac
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-07 19:29:13 -0800

    Add PATCH /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 let express = require('express');
 let bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -70,6 +71,30 @@ app.delete('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((err) => {
     res.send(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (err) => {
+    res.status(400).send();
   });
 });
 
